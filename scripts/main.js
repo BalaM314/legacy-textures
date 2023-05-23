446854e5226ec2c0b6e5b598457c15e22e99491b
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-05-23 16:15:57 +0530

    Remove salvo animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 Events.on(EventType.ContentInitEvent, cons(() => {
 	Blocks.salvo.drawer.parts.clear();
-	const part = new RegionPart("-barrel");
-	part.progress = DrawPart.PartProgress.recoil.delay(0.5);
-	part.under = true;
-	part.turretHeatLayer = Layer.turret - 0.0001;
-	part.moveY = -1.5;
-	Blocks.salvo.drawer.parts.add(part);
+	// Revert to pre-animation because I can't get the texture to look right
+	// const part = new RegionPart("-barrel");
+	// part.progress = DrawPart.PartProgress.recoil.delay(0.5);
+	// part.under = true;
+	// part.turretHeatLayer = Layer.turret - 0.0001;
+	// part.moveY = -1.5;
+	// Blocks.salvo.drawer.parts.add(part);
+
 }));
