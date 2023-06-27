77056deb9d29f30d7e6434c4d30566bd6223d4b6
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-06-27 20:58:57 +0530

    Fix salvo!

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 Events.on(EventType.ContentInitEvent, cons(() => {
 	Blocks.salvo.drawer.parts.clear();
+	//weird hack
+	//attempting to override "salvo" results in an error because vanilla mindustry no longer has a sprite called "salvo"
+	//so i must manually set salvo's texture region
+	Blocks.salvo.region = Core.atlas.find("salvo-preview");
+	//same for salvo-heat, gotta use salvo-barrel-heat instead
+	Blocks.salvo.drawer.heat = Core.atlas.find("salvo-barrel-heat");
+
 	// Revert to pre-animation because I can't get the texture to look right
 	// const part = new RegionPart("-barrel");
 	// part.progress = DrawPart.PartProgress.recoil.delay(0.5);
